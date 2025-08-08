b489977d1e73c5f371eb22ded490e78a112ca486
wangzhizhong <wangzhizhong@qiyi.com>
2025-08-08 13:44:05 +0800

    1

--- backend/main.py
+++ backend/main.py
@@ -1206,8 +1206,8 @@ if __name__ == '__main__':
     except ValueError as e:
         subtitle_area = None
     # 新建字幕提取对象
-    video_path = "G:\workspace\无言之吻.mp4"
-    y_min, y_max, x_min, x_max = 1300, 1600, 0, 1080
+    video_path = "G:\\workspace\\无言之吻.mp4"
+    y_min, y_max, x_min, x_max = 1350, 1600, 0, 1080
     subtitle_area = (y_min, y_max, x_min, x_max)
     se = SubtitleExtractor(video_path, subtitle_area)
     # 开始提取字幕
